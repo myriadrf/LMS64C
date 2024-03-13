569ba49a308d8550dd82794aa5fe40ec01bb4164
Dovydas Rušinskas <dovydasru@gmail.com>
2024-03-13 10:29:48 +0200

    Update LMS64C_protocol.h

--- src/LMS64C_protocol.h
+++ src/LMS64C_protocol.h
@@ -2,9 +2,9 @@
 -- ----------------------------------------------------------------------------	
 -- FILE:	LMS64C_protocol.h
 -- DESCRIPTION:	LMS64C - fixed lenght (64 bytes) lenght control protocol incuding 8 bytes header
--- DATE:	2017.01.11
+-- DATE:	2018.08.03
 -- AUTHOR(s):	Lime Microsystems
--- REVISION: v0r29
+-- REVISION: v0r30
 -- ----------------------------------------------------------------------------	
 
 */
@@ -12,29 +12,36 @@
 #ifndef _LMS64C_PROTOCOL_H_
 #define _LMS64C_PROTOCOL_H_
 
+#define LMS_PROTOCOL_VER		1
+#define LMS_CTRL_PACKET_SIZE	64
+
 enum eLMS_DEV {
-	LMS_DEV_UNKNOWN, 
-	LMS_DEV_EVB6,
-	LMS_DEV_DIGIGREEN, 
-	LMS_DEV_DIGIRED, //2x USB3, LMS6002, 
-	LMS_DEV_EVB7, 
-	LMS_DEV_ZIPPER, //MyRiad bridge to FMC, HSMC bridge
-	LMS_DEV_SOCKETBOARD, 
-	LMS_DEV_EVB7V2, 
-	LMS_DEV_STREAM, //Altera Cyclone IV, USB3, 2x 128 MB RAM, RFDIO, FMC
-	LMS_DEV_NOVENA, //Freescale iMX6 CPU
-	LMS_DEV_DATASPARK, //Altera Cyclone V, 2x 256 MB RAM, 2x FMC (HPC, LPC), USB3
-	LMS_DEV_RFSPARK, //LMS7002 EVB
-	LMS_DEV_LMS6002USB, //LM6002-USB (USB stick: FX3, FPGA, LMS6002, RaspberryPi con)
-	LMS_DEV_RFESPARK, //LMS7002 EVB
-	LMS_DEV_LIMESDR_USB, //LimeSDR-USB, 32bit FX3, 2xRAM, LMS7
-	LMS_DEV_LIMESDR_PCIE,
-	LMS_DEV_LIMESDR_QPCIE, //2x LMS, 14 bit ADC and DAC
-	LMS_DEV_ULIMESDR, //FTDI + MAX10 + LMS
-	LMS_DEV_USTREAM, //with expansion booards (uMyriad)
-	LMS_DEV_LIMESDR_SONY_PA, //stand alone board with Sony PAs, tuners
-	LMS_DEV_LIMESDR_USB_SP,
-	LMS_DEV_LMS7002M_ULTIMATE_EVB,
+	LMS_DEV_UNKNOWN					= 0, 
+	LMS_DEV_EVB6					= 1,
+	LMS_DEV_DIGIGREEN				= 2, 
+	LMS_DEV_DIGIRED					= 3, //2x USB3, LMS6002, 
+	LMS_DEV_EVB7					= 4, 
+	LMS_DEV_ZIPPER					= 5, //MyRiad bridge to FMC, HSMC bridge
+	LMS_DEV_SOCKETBOARD				= 6, 
+	LMS_DEV_EVB7V2					= 7, 
+	LMS_DEV_STREAM					= 8, //Altera Cyclone IV, USB3, 2x 128 MB RAM, RFDIO, FMC
+	LMS_DEV_NOVENA					= 9, //Freescale iMX6 CPU
+	LMS_DEV_DATASPARK				= 10, //Altera Cyclone V, 2x 256 MB RAM, 2x FMC (HPC, LPC), USB3
+	LMS_DEV_RFSPARK					= 11, //LMS7002 EVB
+	LMS_DEV_LMS6002USB				= 12, //LM6002-USB (USB stick: FX3, FPGA, LMS6002, RaspberryPi con)
+	LMS_DEV_RFESPARK				= 13, //LMS7002 EVB
+	LMS_DEV_LIMESDR_USB				= 14, //LimeSDR-USB, 32bit FX3, 2xRAM, LMS7
+	LMS_DEV_LIMESDR_PCIE			= 15,
+	LMS_DEV_LIMESDR_QPCIE			= 16, //2x LMS, 14 bit ADC and DAC
+	LMS_DEV_ULIMESDR				= 17, //FTDI + MAX10 + LMS
+	LMS_DEV_USTREAM					= 18, //with expansion booards (uMyriad)
+	LMS_DEV_LIMESDR_SONY_PA			= 19, //stand alone board with Sony PAs, tuners
+	LMS_DEV_LIMESDR_USB_SP			= 20,
+	LMS_DEV_LMS7002M_ULTIMATE_EVB 	= 21,
+	LMS_DEV_LIMENET_MICRO			= 22, //Raspberry Pi CM3(L), Ethernet, MAX10, LMS7002, 
+	LMS_DEV_LIMESDR_CORE_SDR		= 23, //LMS7002, Intel Cyclone 4, RAM, GNSS
+	LMS_DEV_LIMESDR_CORE_HE			= 24, //PA board
+	
 	};
 
 enum eEXP_BOARD {
@@ -49,9 +56,6 @@ enum eEXP_BOARD {
 	EXP_BOARD_HPM7, //Myriad long with LMS7002, 2x PA, matching, DAC
 	EXP_BOARD_MYRIAD7_NOVENA, //Novena myriad with LMS7002
 	};
-
-#define LMS_PROTOCOL_VER		1
-#define LMS_CTRL_PACKET_SIZE	64
 
 //commands
 #define CMD_GET_INFO		0x00 //Returns some info about board and firmware
