be50087b55d9627b9b700ab1278622e1a92244c2
Dovydas Rušinskas <dovydasru@gmail.com>
2024-03-13 10:27:39 +0200

    Update LMS64C_protocol.h

--- src/LMS64C_protocol.h
+++ src/LMS64C_protocol.h
@@ -2,9 +2,9 @@
 -- ----------------------------------------------------------------------------	
 -- FILE:	LMS64C_protocol.h
 -- DESCRIPTION:	LMS64C - fixed lenght (64 bytes) lenght control protocol incuding 8 bytes header
--- DATE:	2015.06.01
+-- DATE:	2015.11.28
 -- AUTHOR(s):	Lime Microsystems
--- REVISION: v0r19
+-- REVISION: v0r23
 -- ----------------------------------------------------------------------------	
 
 */
@@ -16,7 +16,7 @@ enum eLMS_DEV {
 	LMS_DEV_UNKNOWN, 
 	LMS_DEV_EVB6,
 	LMS_DEV_DIGIGREEN, 
-	LMS_DEV_DIGIRED, //2x LMS6002, USB3
+	LMS_DEV_DIGIRED, //2x USB3, LMS6002, 
 	LMS_DEV_EVB7, 
 	LMS_DEV_ZIPPER, //MyRiad bridge to FMC, HSMC bridge
 	LMS_DEV_SOCKETBOARD, 
@@ -24,8 +24,10 @@ enum eLMS_DEV {
 	LMS_DEV_STREAM, //Altera Cyclone IV, USB3, 2x 128 MB RAM, RFDIO, FMC
 	LMS_DEV_NOVENA, //Freescale iMX6 CPU
 	LMS_DEV_DATASPARK, //Altera Cyclone V, 2x 256 MB RAM, 2x FMC (HPC, LPC), USB3
-	LMS_DEV_RFSPARK, //LMS7002
+	LMS_DEV_RFSPARK, //LMS7002 EVB
 	LMS_DEV_LMS6002USB, //LM6002-USB (USB stick: FX3, FPGA, LMS6002, RaspberryPi con)
+	LMS_DEV_RFESPARK, //LMS7002 EVB
+	LMS_DEV_SODERA, //SoDeRa, 32bit FX3, 2xRAM, LMS7
 	};
 
 enum eEXP_BOARD {
@@ -34,10 +36,11 @@ enum eEXP_BOARD {
 	EXP_BOARD_NO, //device does not supoort any exp boards
 	EXP_BOARD_MYRIAD1, //myriad1 LMS6002
 	EXP_BOARD_MYRIAD2, //myriad2 LMS6002, longer board, with PA, MIPI
-	EXP_BOARD_MYRIAD_NOVENA, //Novena myriad
+	EXP_BOARD_MYRIAD_NOVENA, //Novena myriad with LMS6002
 	EXP_BOARD_HPM1000, //myriad with MCU, PA, MPI
 	EXP_BOARD_MYRIAD7, //Myriad with LMS7002
-	EXP_BOARD_HPM7, //Myriad long with LMS7002, 2x PA
+	EXP_BOARD_HPM7, //Myriad long with LMS7002, 2x PA, matching, DAC
+	EXP_BOARD_MYRIAD7_NOVENA, //Novena myriad with LMS7002
 	};
 
 #define LMS_PROTOCOL_VER		1
@@ -51,8 +54,8 @@ enum eEXP_BOARD {
 #define CMD_SI5356_RD		0x12
 #define CMD_SI5351_WR	 	0x13 //Writes data to SI5351 (clock generator) via I2C
 #define CMD_SI5351_RD		0x14 //Reads data from SI5351 (clock generator) via I2C
-#define CMD_TFP410_WR		0x15 //PanelBus DVI (HDMI) Transmitter control
-#define CMD_TFP410_RD		0x16 //PanelBus DVI (HDMI) Transmitter control
+#define CMD_TFP410_WR		0x15 //PanelBus DVI (HDMI) Transmitter control //remove?
+#define CMD_TFP410_RD		0x16 //PanelBus DVI (HDMI) Transmitter control //remove?
 
 #define CMD_LMS_RST			0x20 //CMD_LMS_RST
 #define CMD_LMS7002_WR		0x21 //Writes data to LMS7002 chip via SPI (16 bits word)
@@ -62,14 +65,14 @@ enum eEXP_BOARD {
 
 #define CMD_LMS_LNA			0x2A
 #define CMD_LMS_PA			0x2B
-#define CMD_PROG_MCU		0x2C //LMS7 MCU FW write
+#define CMD_LMS_MCU_FW_WR	0x2C //LMS7 MCU FW write
 #define CMD_WR_MCU			0x2D //testing
 #define CMD_RD_MCU			0x2E //testing
 
 //ADF4002
 #define CMD_ADF4002_WR		0x31 //Writes data to ADF4002 (phase detector/frequency synthesizer) via SPI
 
-#define CMD_FX2				0x40 //FX2 Configuration, resets endpoints (DigiGreen)
+#define CMD_SSTREAM_RST				0x40 //FX2 Configuration, resets endpoints (DigiGreen)
 
 #define CMD_PE636040_WR		0x41 //Writes data to PE636040 tuner
 #define CMD_PE636040_RD		0x42 //Reads data from PE636040 tuner
@@ -79,7 +82,7 @@ enum eEXP_BOARD {
 #define CMD_ALTERA_FPGA_GW_WR		0x53
 #define CMD_ALTERA_FPGA_GW_RD		0x54
 
-//spi peripherals control
+//onboard spi peripherals control
 #define CMD_BRDSPI16_WR		0x55 //16 + 16 bit spi for stream, dataspark control
 #define CMD_BRDSPI16_RD		0x56 //16 + 16 bit spi for stream, dataspark control
 #define CMD_BRDSPI8_WR		0x57 //8 + 8 bit spi for stream, dataspark control
@@ -90,6 +93,7 @@ enum eEXP_BOARD {
 
 #define CMD_ANALOG_VAL_WR	0x61 //write analog value
 #define CMD_ANALOG_VAL_RD	0x62 //read analog value
+//read reference??? ofset? min? max?
 
 //0x6x free?
 //0x7x free?
@@ -98,15 +102,12 @@ enum eEXP_BOARD {
 #define 16bit spi wr, rd
 #define 8bit i2c
 #*/
-
-#define CMD_MYRIAD_RST		0x80
-#define CMD_MYRIAD_WR		0x81
-#define CMD_MYRIAD_RD		0x82
-#define CMD_MYRIAD_FW_WR	0x8C //Writes FW to MyRiad board via SPI
-#define CMD_MYRIAD_FW_RD	0x8D //Reads FW from MyRiad board via SPI
-
-
-
+//RFDIO (EXP) board
+#define CMD_EXP_BRD_RST		0x80
+#define CMD_EXP_BRDSPI8_WR	0x81
+#define CMD_EXP_BRDSPI8_RD	0x82
+#define CMD_EXP_BRD_FW_WR	0x8C //Writes FW to MyRiad board via SPI
+#define CMD_EXP_BRD_FW_RD	0x8D //Reads FW from MyRiad board via SPI
 
 //status
 #define STATUS_COMPLETED_CMD		1 //Command successfully executed
@@ -116,6 +117,9 @@ enum eEXP_BOARD {
 #define STATUS_ERROR_CMD			5 //Error occurred during command execution
 #define STATUS_WRONG_ORDER_CMD		6 //Broken packets sequence
 #define STATUS_RESOURCE_DENIED_CMD	7 //Board resource denied
+#define STATUS_INVALID_PERIPH_ID_CMD	8 //
+
+enum STATUS_BRD {STATUS_BRD_UNKNOWN, STATUS_BRD_READY, STATUS_BRD_BUSY, STATUS_BRD_ERROR};
 
 //CMD_LMS_RST
 #define LMS_RST_DEACTIVATE	0
@@ -126,7 +130,8 @@ typedef struct{
     unsigned char Command;
     unsigned char Status;
 	unsigned char Data_blocks;
-	unsigned char reserved[5];
+	unsigned char Periph_ID;
+	unsigned char reserved[4];
 }tLMS_Ctrl_Header;
 
 typedef struct{
